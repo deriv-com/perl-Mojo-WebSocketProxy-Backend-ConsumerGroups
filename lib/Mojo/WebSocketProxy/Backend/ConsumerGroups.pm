7e110e5a5a3ed2a91a4aeb741180b332a85229fd
chylli-binary <chylli@binary.com>
2022-01-06 21:19:29 +0800

    init

---
+++ lib/Mojo/WebSocketProxy/Backend/ConsumerGroups.pm
@@ -0,0 +1,36 @@
+package Mojo::WebSocketProxy::Backend::ConsumerGroups;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Mojo::WebSocketProxy::Backend::ConsumerGroups - Module abstract
+
+=head1 SYNOPSIS
+
+    use Mojo::WebSocketProxy::Backend::ConsumerGroups;
+    my $instance = Mojo::WebSocketProxy::Backend::ConsumerGroups->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
